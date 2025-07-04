2d4c55d1832c531e3b23369674915d2f8d3c3cb8
BohdanVoitsehivsky <bogdanvoitsehivsky@gmail.com>
2025-07-04 12:55:50 +0300

    lesson 01.07

--- js/03-product.js
+++ js/03-product.js
@@ -14,73 +14,73 @@
 
 
 
-const products = [
-    {
-      id: 1,
-      img: "https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG",
-      name: "Monitor",
-      price: 3000,
-      description: "23-inch monitor with Full HD resolution.",
-    },
-    {
-      id: 2,
-      img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzWqRMI3HQiDfICHAmbArmaP4uOOIjfz0sDITv0dfkpb0mbbgX",
-      name: "Laptop",
-      price: 20000,
-      description:
-        "Lightweight and powerful laptop with a 15-inch display and SSD.",
-    },
-    {
-      id: 3,
-      img: "https://cdn.27.ua/799/66/39/6841913_1.jpeg",
-      name: "Smartphone",
-      price: 8000,
-      description: "Equipped with a triple camera and a multi-core processor.",
-    },
-    {
-      id: 4,
-      img: "https://cdn.27.ua/799/b6/16/4371990_1.jpeg",
-      name: "Tablet",
-      price: 12000,
-      description: "10-inch tablet with high performance and a Retina display.",
-    },
-];
+// const products = [
+//     {
+//       id: 1,
+//       img: "https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG",
+//       name: "Monitor",
+//       price: 3000,
+//       description: "23-inch monitor with Full HD resolution.",
+//     },
+//     {
+//       id: 2,
+//       img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzWqRMI3HQiDfICHAmbArmaP4uOOIjfz0sDITv0dfkpb0mbbgX",
+//       name: "Laptop",
+//       price: 20000,
+//       description:
+//         "Lightweight and powerful laptop with a 15-inch display and SSD.",
+//     },
+//     {
+//       id: 3,
+//       img: "https://cdn.27.ua/799/66/39/6841913_1.jpeg",
+//       name: "Smartphone",
+//       price: 8000,
+//       description: "Equipped with a triple camera and a multi-core processor.",
+//     },
+//     {
+//       id: 4,
+//       img: "https://cdn.27.ua/799/b6/16/4371990_1.jpeg",
+//       name: "Tablet",
+//       price: 12000,
+//       description: "10-inch tablet with high performance and a Retina display.",
+//     },
+// ];
 
 
 
-const container = document.querySelector(".products");
-container.insertAdjacentHTML("beforeend", createMarkup(products))
+// const container = document.querySelector(".products");
+// container.insertAdjacentHTML("beforeend", createMarkup(products))
 
-function createMarkup(arr) {
-    return arr.map(item => `<li class="item product-item" data-id="${item.id}"><img src="${item.img}" alt="${item.name}" width="300"/>
-        <h2>${item.name}</h2>
-        <p>Ціна: ${item.price} грн</p>
-        </li>`).join("");
-}
+// function createMarkup(arr) {
+//     return arr.map(item => `<li class="item product-item" data-id="${item.id}"><img src="${item.img}" alt="${item.name}" width="300"/>
+//         <h2>${item.name}</h2>
+//         <p>Ціна: ${item.price} грн</p>
+//         </li>`).join("");
+// }
 
-container.addEventListener("click", handleClick);
-function handleClick(event) {
- if(event.currentTarget === event.target) {
-        return;
-    };
-const parent = event.target.closest(".product-item");
-const productId = parent.dataset.id;
-const product = products.find((item) => item.id === Number(productId));
-console.log(productId);
+// container.addEventListener("click", handleClick);
+// function handleClick(event) {
+//  if(event.currentTarget === event.target) {
+//         return;
+//     };
+// const parent = event.target.closest(".product-item");
+// const productId = parent.dataset.id;
+// const product = products.find((item) => item.id === Number(productId));
+// console.log(productId);
 
-const instance = basicLightbox.create(`
-	<div class="modal">
-    <img src="${product.img}" alt="${product.name}" />
-    <h2>${product.name}</h2>
-    <h3>${product.price}</h3>
-    <p>${product.description}</p>
-    </div>
-`)
-
-
-instance.show();
+// const instance = basicLightbox.create(`
+// 	<div class="modal">
+//     <img src="${product.img}" alt="${product.name}" />
+//     <h2>${product.name}</h2>
+//     <h3>${product.price}</h3>
+//     <p>${product.description}</p>
+//     </div>
+// `)
 
 
- }
+// instance.show();
+
+
+//  }
    
     
---
+++ js/practice-01.07.js
@@ -0,0 +1,34 @@
+// ****** local storage
+
+// const LS_KEY = "Array of names";
+// const names = ["Alice", "Kate", "Emma"];
+
+
+// // *** Збереженяня
+// // *** чому треба використовувати метод JSON.stringify
+// localStorage.setItem(LS_KEY, JSON.stringify(names));
+
+// const value = localStorage.getItem(LS_KEY)
+// console.log(Array.isArray(JSON.parse(value)));
+
+
+// **** remove
+
+// localStorage.removeItem("Array of names")
+// localStorage.clear()
+
+
+
+
+// ***** localStorage ен можу функції
+
+// const calc = {
+//     a: 5,
+//     b: 10,
+//     add() {
+//         return this.a + this.b
+//     }
+// }
+// localStorage.setItem("foo", JSON.stringify(calc))
+// const value = localStorage.getItem("foo")
+// console.log(JSON.parse(value));
